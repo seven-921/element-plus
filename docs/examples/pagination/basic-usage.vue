f003d108782c497cbc6e03872c0443350ce1257d
seven-921 <126318797+seven-921@users.noreply.github.com>
2023-06-29 14:19:04 +0800

    Update basic-usage.vue

--- docs/examples/pagination/basic-usage.vue
+++ docs/examples/pagination/basic-usage.vue
@@ -5,7 +5,7 @@
   </div>
   <div class="example-pagination-block">
     <div class="example-demonstration">When you have more than 7 pages</div>
-    <el-pagination layout="prev, pager, next" :total="1000" />
+    <el-pagination layout="prev, pager, next" :total="100000000" />
   </div>
 </template>
 
